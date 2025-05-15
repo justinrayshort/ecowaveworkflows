2b12473bd467ea2490e8a188584f669b9b2305ed
Justin Short <justin@jshort.org>
2025-05-14 18:26:16 -0700

    edited index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex items-center justify-center">
+	<div class="w-full sm:w-1/2 md:max-w-1/2">
+		<img alt="AI technology" src="/ai2.jpg" />
+		<div class="hero-content text-neutral-content text-center">
+			<div class="">
+				<h1 class="mb-2 text-5xl font-bold">EcoWave Workflows</h1>
+				<p class="p-4">
+					We specialize in designing workflows that are custom tailored to your business needs.
+				</p>
+				<p class="p-4">
+					Our team of experts will work with you to create a customized solution that streamlines
+					your processes and improves efficiency. With EcoWave, you can expect a seamless
+					integration of technology and human interaction, resulting in a more productive and
+					satisfying experience for both you and your customers.
+				</p>
+				<p class="mb-4 p-4">Click below to contact one of our team members!</p>
+				<button class="btn btn-primary"
+					><a href="mailto:sales@ecowaveworkflows.com">Get Started!</a></button
+				>
+			</div>
+		</div>
+	</div>
+</div>
